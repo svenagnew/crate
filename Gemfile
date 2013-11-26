11bf54bdc1e85ab628c0e54a7b5575070f9d7552
Sven Agnew <sven@kangohammer.com>
2013-11-26 13:06:39 +0200

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+
+gem 'rspec'
+gem 'guard-rspec'
+gem 'launchy'
+gem 'capybara'
+
---
+++ brew-versions.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby -wKU
+#
+# A little utility script to backup homebrew installed packages at specific versions.
+# The Regex: /^[a-zA-Z]*:\s?stable\s?.*$/
+
+# Setup a few variables for the script, tmp files and such
+
+# Generate a list of already installed packages
+
+# Get extended version information for each installed package
+
+# Hunt for the package and version string in the versions file
